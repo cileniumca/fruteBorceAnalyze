a84a73490c1d4745a8cd80acb02f7a816e307a33
Sergey Bykanov <sbykanov@admixer.ua>
2025-06-08 18:23:39 +0300

    maxParallelism

--- AnalyzeDomains.Infrastructure/Services/BatchProcessorWorker.cs
+++ AnalyzeDomains.Infrastructure/Services/BatchProcessorWorker.cs
@@ -23,7 +23,7 @@ namespace AnalyzeDomains.Infrastructure.Services
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var maxParallelism = Environment.ProcessorCount*512;
+            var maxParallelism = Environment.ProcessorCount*1024;
             if (maxParallelism < 1) 
                 maxParallelism = 1;
 
